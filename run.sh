d009e207e32f812047c388144c8d056443181538
com2u <paddy22@gmx.de>
2024-12-28 01:16:00 +0100

    build script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 docker stop daily-routine-schedule-app
 docker rm -f daily-routine-schedule-app
-docker run --name ai-daily-routine-schedule-app --restart unless-stopped -p 5748:5000 daily-routine-schedule-app
+docker run --name daily-routine-schedule-app -v ~/daily-routine-schedule-app/instance:/instance --restart unless-stopped -p 5748:5000 daily-routine-schedule-app
