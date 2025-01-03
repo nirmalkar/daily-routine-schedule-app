ff943e36f9f8c514a896b39456896939dd5432b9
com2u <paddy22@gmx.de>
2025-01-03 16:22:33 +0100

    .env outside docker

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ chmod +x run.sh
 chmod +x build.sh
 docker stop daily-routine-schedule-app
 docker rm -f daily-routine-schedule-app
-docker run --name daily-routine-schedule-app -v ~/daily-routine-schedule-app/daily_routine.db:/instance/daily_routine.db --restart unless-stopped -p 5748:5000 -p 5749:3000 daily-routine-schedule-app
+docker run --name daily-routine-schedule-app -v $(pwd)/.env:/app/.env -v $(pwd)/instance:/app/instance --restart unless-stopped -p 5748:5000 -p 5749:3000 daily-routine-schedule-app
